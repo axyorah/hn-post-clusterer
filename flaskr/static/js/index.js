0d0fdfe85e13bf48d8aee1949b18d906e1d1e1e2
axyorah <elvira.bozileva@gmail.com>
2021-07-26 16:00:37 +0200

    mod: reorganized event listeners for form elements - now all form fields are stored as properties on an object, so it's a bit easier to 'scale-up'

--- flaskr/static/js/index.js
+++ flaskr/static/js/index.js
@@ -1,24 +1,33 @@
 container = document.querySelector('.container');
 
-seedBeginDateRange = document.querySelector('#seed-begin-date-range');
-seedEndDateRange = document.querySelector('#seed-end-date-range');
-seedBeginDateLabel = document.querySelector('#seed-begin-date-label');
-seedEndDateLabel = document.querySelector('#seed-end-date-label');
+const seed = new Object();
+const show = new Object();
 
-seedBeginIdRange = document.querySelector('#seed-begin-id-range');
-seedEndIdRange = document.querySelector('#seed-end-id-range');
-seedBeginIdLabel = document.querySelector('#seed-begin-id-label');
-seedEndIdLabel = document.querySelector('#seed-end-id-label');
+for (let filterBy of ['date', 'id']) {
+    if (seed[filterBy] === undefined) {
+        seed[filterBy] = new Object();
+    }
+    for (let loc of ['begin', 'end']) {
+        if (seed[filterBy][loc] === undefined) {
+            seed[filterBy][loc] = new Object();
+        }
+        seed[filterBy][loc]['range'] = document.querySelector(`#seed-${loc}-${filterBy}-range`);
+        seed[filterBy][loc]['label'] = document.querySelector(`#seed-${loc}-${filterBy}-label`);
+    }
+}
 
-showBeginDateRange = document.querySelector('#show-begin-date-range');
-showEndDateRange = document.querySelector('#show-end-date-range');
-showBeginDateLabel = document.querySelector('#show-begin-date-label');
-showEndDateLabel = document.querySelector('#show-end-date-label');
-
-showBeginIdRange = document.querySelector('#show-begin-id-range');
-showEndIdRange = document.querySelector('#show-end-id-range');
-showBeginIdLabel = document.querySelector('#show-begin-id-label');
-showEndIdLabel = document.querySelector('#show-end-id-label');
+for (let filterBy of ['date', 'id', 'comm', 'score']) {
+    if (show[filterBy] === undefined) {
+        show[filterBy] = new Object();
+    }
+    for (let loc of ['begin', 'end']) {
+        if (show[filterBy][loc] === undefined) {
+            show[filterBy][loc] = new Object();
+        }
+        show[filterBy][loc]['range'] = document.querySelector(`#show-${loc}-${filterBy}-range`);
+        show[filterBy][loc]['label'] = document.querySelector(`#show-${loc}-${filterBy}-label`);
+    }
+}
 
 // Globals
 const minTS = new Date(2006, 10, 9).valueOf();
@@ -29,137 +38,105 @@ const maxID = 27752765
 
 
 // Helpers
-configDateRangesAndLabels = function (beginRange, endRange, beginLabel, endLabel) {
-    beginRange.min = minTS;
-    beginRange.max = maxTS;
-    beginRange.value = minTS;
+const config = {
+    date: function (beginRange, endRange, beginLabel, endLabel) {
+        beginRange.min = minTS;
+        beginRange.max = maxTS;
+        beginRange.value = minTS;
+    
+        endRange.min = minTS;
+        endRange.max = maxTS;
+        endRange.value = maxTS;
+    
+        const [m0, d0, y0] = new Date(minTS).toLocaleDateString().split('/');
+        const [mf, df, yf] = new Date(maxTS).toLocaleDateString().split('/');
+        beginLabel.innerText = `From: ${y0}/${m0}/${d0}`;
+        endLabel.innerText = `To: ${yf}/${mf}/${df}`;
+    },
 
-    endRange.min = minTS;
-    endRange.max = maxTS;
-    endRange.value = maxTS;
-
-    const [m0, d0, y0] = new Date(minTS).toLocaleDateString().split('/');
-    const [mf, df, yf] = new Date(maxTS).toLocaleDateString().split('/');
-    beginLabel.innerText = `From: ${y0}/${m0}/${d0}`;
-    endLabel.innerText = `To: ${yf}/${mf}/${df}`;
-}
-
-configIdRangesAndLabels = function (beginRange, endRange, beginLabel, endLabel) {
-    beginRange.min = minID;
-    beginRange.max = maxID;
-    beginRange.value = minID;
-
-    endRange.min = minID;
-    endRange.max = maxID;
-    endRange.value = maxID;
-
-    beginLabel.innerText = `From: ${minID}`;
-    endLabel.innerText = `To: ${maxID}`;
-}
-
-updateBeginDateRange = function (beginRange, endRange, beginLabel, endLabel) {
-    let beginVal = parseInt(beginRange.value);
-    let endVal = parseInt(endRange.value);
-    if (beginVal >= endVal) {
-        beginVal = endVal - 1000 * 60 * 60 * 24;
-        beginRange.value = beginVal;
+    id: function (beginRange, endRange, beginLabel, endLabel) {
+        beginRange.min = minID;
+        beginRange.max = maxID;
+        beginRange.value = minID;
+    
+        endRange.min = minID;
+        endRange.max = maxID;
+        endRange.value = maxID;
+    
+        beginLabel.innerText = `From: ${minID}`;
+        endLabel.innerText = `To: ${maxID}`;
     }
-    const [m, d, y] = new Date(beginVal).toLocaleDateString().split('/');
-    beginLabel.innerText = `From: ${y}/${m}/${d}`;
-}
+};
 
-updateEndDateRange = function (beginRange, endRange, beginLabel, endLabel) {
-    let beginVal = parseInt(beginRange.value);
-    let endVal = parseInt(endRange.value);
-    if (endVal <= beginVal) {
-        endVal = beginVal + 1000 * 60 * 60 * 24;
-        endDateRange.value = endVal;
-    }
-    const [m, d, y] = new Date(endVal).toLocaleDateString().split('/');
-    endLabel.innerText = `To: ${y}/${m}/${d}`;
-}
+const update = {
+    date: {
+        begin: function(beginRange, endRange, beginLabel, endLabel) {
+            let beginVal = parseInt(beginRange.value);
+            let endVal = parseInt(endRange.value);
+            if (beginVal >= endVal) {
+                beginVal = endVal - 1000 * 60 * 60 * 24;
+                beginRange.value = beginVal;
+            }
+            const [m, d, y] = new Date(beginVal).toLocaleDateString().split('/');
+            beginLabel.innerText = `From: ${y}/${m}/${d}`;
+        },
+        
+        end: function(beginRange, endRange, beginLabel, endLabel) {
+            let beginVal = parseInt(beginRange.value);
+            let endVal = parseInt(endRange.value);
+            if (endVal <= beginVal) {
+                endVal = beginVal + 1000 * 60 * 60 * 24;
+                endDateRange.value = endVal;
+            }
+            const [m, d, y] = new Date(endVal).toLocaleDateString().split('/');
+            endLabel.innerText = `To: ${y}/${m}/${d}`;            
+        }
+    }, 
 
-updateBeginIdRange = function(beginRange, endRange, beginLabel, endLabel) {
-    let beginVal = parseInt(beginRange.value);
-    let endVal = parseInt(endRange.value);
-    if (beginVal >= endVal) {
-        beginVal = endVal - 1;
-        beginRange.value = beginVal;
-    }
-    beginLabel.innerText = `From: ${beginVal}`;
-}
+    id: {
+        begin: function(beginRange, endRange, beginLabel, endLabel) {
+            let beginVal = parseInt(beginRange.value);
+            let endVal = parseInt(endRange.value);
+            if (beginVal >= endVal) {
+                beginVal = endVal - 1;
+                beginRange.value = beginVal;
+            }
+            beginLabel.innerText = `From: ${beginVal}`;            
+        }, 
 
-updateEndIdRange = function (beginRange, endRange, beginLabel, endLabel) {
-    let beginVal = parseInt(beginRange.value);
-    let endVal = parseInt(endRange.value);
-    if (endVal <= beginVal) {
-        endVal = beginVal + 1;
-        endRange.value = endVal;
+        end: function(beginRange, endRange, beginLabel, endLabel) {
+            let beginVal = parseInt(beginRange.value);
+            let endVal = parseInt(endRange.value);
+            if (endVal <= beginVal) {
+                endVal = beginVal + 1;
+                endRange.value = endVal;
+            }
+            endLabel.innerText = `To: ${endVal}`;            
+        }
     }
-    endLabel.innerText = `To: ${endVal}`;
 }
 
 
 // Event Listeners
-seedBeginDateRange.addEventListener('change', (evt) => {
-    updateBeginDateRange(
-        seedBeginDataRange, seedEndDataRange, seedBeginDateLabel, seedEndDateLabel
-    );
-})
+for (let form of [seed, show]) {
+    for (let filterBy of ['date', 'id']) {
+        for (let loc of ['begin', 'end']) {
 
-seedEndDateRange.addEventListener('change', (evt) => {
-    updateEndDateRange(
-        seedBeginDateRange, seedEndDateRange, seedBeginDateLabel, seedEndDataLabel
-    );
-})
+            form[filterBy][loc].range.addEventListener('change', (evt) => {
+                update[filterBy][loc](
+                    form[filterBy].begin.range, form[filterBy].end.range, form[filterBy].begin.label, form[filterBy].end.label
+                );
+            })
 
-showBeginDateRange.addEventListener('change', (evt) => {
-    updateBeginDateRange(
-        showBeginDataRange, showEndDataRange, showBeginDateLabel, showEndDateLabel
-    );
-})
-
-showEndDateRange.addEventListener('change', (evt) => {
-    updateEndDateRange(
-        showBeginDateRange, showEndDateRange, showBeginDateLabel, showEndDataLabel
-    );
-})
-
-seedBeginIdRange.addEventListener('change', (evt) => {
-    updateBeginIdRange(
-        seedBeginIdRange, seedEndIdRange, seedBeginIdLabel, seedEndIdLabel
-    );
-})
-
-seedEndIdRange.addEventListener('change', (evt) => {
-    updateEndIdRange(
-        seedBeginIdRange, seedEndIdRange, seedBeginIdLabel, seedEndIdLabel
-    )
-})
-
-showBeginIdRange.addEventListener('change', (evt) => {
-    updateBeginIdRange(
-        showBeginIdRange, showEndIdRange, showBeginIdLabel, showEndIdLabel
-    );
-})
-
-showEndIdRange.addEventListener('change', (evt) => {
-    updateEndIdRange(
-        showBeginIdRange, showEndIdRange, showBeginIdLabel, showEndIdLabel
-    )
-})
+        }
+    }
+}
 
 window.addEventListener('load', (evt) => {
-    configDateRangesAndLabels(
-        seedBeginDateRange, seedEndDateRange, seedBeginDateLabel, seedEndDateLabel
-    );
-    configDateRangesAndLabels(
-        showBeginDateRange, showEndDateRange, showBeginDateLabel, showEndDateLabel
-    );
-    configIdRangesAndLabels(
-        seedBeginIdRange, seedEndIdRange, seedBeginIdLabel, seedEndIdLabel
-    );
-    configIdRangesAndLabels(
-        showBeginIdRange, showEndIdRange, showBeginIdLabel, showEndIdLabel
-    );
+    
+    for (let form of [seed, show]) {
+        for (let filterBy of ['date', 'id']) {
+            config[filterBy](form[filterBy].begin.range, form[filterBy].end.range, form[filterBy].begin.label, form[filterBy].end.label)
+        }
+    }
 })
