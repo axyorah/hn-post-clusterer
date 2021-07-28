826cb1cab3c09e027c1c635b73338f557cbf467f
axyorah <elvira.bozileva@gmail.com>
2021-07-28 18:05:00 +0200

    fix: `<br>` tags in comments are also correctly handled when serializing on disc (replaced by ' ' instead of '')

--- flaskr/static/utils/datautils.py
+++ flaskr/static/utils/datautils.py
@@ -13,7 +13,7 @@ def serialize_to_disc(corpus_dir, rows):
 
         # parse children (story's comments are stored as an html doc)
         soup = bs.BeautifulSoup(dct['children'], 'lxml')
-        dct['children'] = ' '.join(p.text for p in soup.find_all('p'))
+        dct['children'] = ' '.join(p.get_text(separator=' ') for p in soup.find_all('p'))
 
         # write into a separate file
         jsoned = json.dumps(dct)
