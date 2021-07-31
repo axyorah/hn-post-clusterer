0ee36f8e1f6ceb7c606ca3bb900f277f5422b046
axyorah <elvira.bozileva@gmail.com>
2021-07-31 16:44:35 +0200

    mod: `get_document_dict_from_sqlite_rows(.)` now returns dict with all the fields available for the story, with `story_id` acting as a dict key (previously it would only return `children` = all story comments concatenated into a single string)

--- flaskr/static/utils/formutils.py
+++ flaskr/static/utils/formutils.py
@@ -61,8 +61,13 @@ def get_document_dict_from_sqlite_rows(rows) -> 'dict':
     """
     documents = dict()
     for row in rows:
-        documents[row.__getitem__("story_id")] = \
-            f'{row.__getitem__("title")}\t{row.__getitem__("children")}'
+        documents[row.__getitem__('story_id')] = {
+            'author': row.__getitem__('author'),
+            'unix_time': row.__getitem__('unix_time'),
+            'score': row.__getitem__('score'),
+            'title': row.__getitem__('title'),
+            'children': f'{row.__getitem__("title")}\t{row.__getitem__("children")}'
+        }
         
     return documents
 
