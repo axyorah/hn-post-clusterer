f302e3f72e63667124160f84464cd4ac2c862567
axyorah <elvira.bozileva@gmail.com>
2021-08-01 15:39:53 +0200

    mod: now db server is queried for a limited id range at a time; e.g., if server is queried for id range [0,1000000] only the posts corresponding to the first 5000 items will appear in teh table; to get the rest of the results `More!` btn should be pressed several times...

--- flaskr/static/js/index.js
+++ flaskr/static/js/index.js
@@ -21,6 +21,17 @@ for (let filterBy of ['date', 'id', 'comm', 'score']) {
 }
 
 // Vars
+const idRange = 5000;
+class Counter {
+    constructor() {
+        this._count = 0;
+    }
+    get count() { return this._count; }
+    increment = function() {
+        this._count += 1;
+    }
+}
+
 const range = {
     date: {
         min: new Date(2006, 10, 9).valueOf(),
@@ -152,18 +163,49 @@ async function postData(url, data) {
     return response.json(); 
 }
 
-function getHTMLDetails(title, innerHTML) {
-    const details = document.createElement('details');
-    const summary = document.createElement('summary');
-    const p = document.createElement('p');
+function getMorePostsBtn() {
+    const counter = new Counter();
+    const moreBtn = document.createElement('button');
+    moreBtn.innerText = 'More!';
 
-    summary.innerText = title;
-    p.innerHTML = innerHTML;
+    moreBtn.addEventListener('click', function (evt) {
+        // check fraction of posts/comments at a time
+        counter.increment();
 
-    details.appendChild(summary);
-    details.appendChild(p);
+        const idBegin = Math.min(
+            parseInt(show.id.begin.range.value) + counter.count * idRange,
+            parseInt(show.id.end.range.value)
+        );
+        const idEnd = Math.min(
+            parseInt(show.id.begin.range.value) + (counter.count + 1) * idRange,
+            parseInt(show.id.end.range.value)
+        );
 
-    return details;
+        const data = {
+            'show-id-begin-range': idBegin,
+            'show-id-end-range': idEnd,
+            'show-comm-begin-range': show.comm.begin.range.value,
+            'show-comm-end-range': show.comm.end.range.value,
+            'show-score-begin-range': show.score.begin.range.value,
+            'show-score-end-range': show.score.end.range.value,
+            'show-lsi-topics-num': showLsiTopicsNum.value,
+            'show-kmeans-clusters-num': showKmeansClustersNum.value        
+        }
+
+        if (idBegin === show.id.end.range.value) {
+            return;
+        } else {
+            postData('/db', data)
+            .then(res => {
+                console.log(`Posting stuff from #${data['show-id-begin-range']} to #${data['show-id-end-range']}`);
+                appendDataToHNPostTable(tableRoot.children[0], res);
+            })
+            .catch((err) => console.log(err));
+        }        
+
+    })
+
+    return moreBtn;
 }
 
 function getNewHNPostTable() {
@@ -182,7 +224,25 @@ function getNewHNPostTable() {
     }
     table.appendChild(trHead);
 
+    // create `more` button
+    moreBtn = getMorePostsBtn();
+    tableRoot.appendChild(moreBtn);
+
     return table;
+}
+
+function getHTMLDetails(title, innerHTML) {
+    const details = document.createElement('details');
+    const summary = document.createElement('summary');
+    const p = document.createElement('p');
+
+    summary.innerText = title;
+    p.innerHTML = innerHTML;
+
+    details.appendChild(summary);
+    details.appendChild(p);
+
+    return details;
 }
 
 function appendDataToHNPostTable(table, data) {
@@ -208,48 +268,7 @@ function appendDataToHNPostTable(table, data) {
         // add row to table
         table.appendChild(tr);
     }
-
-
 }
-
-// function addDataToTable(data) {
-//     // clear table
-//     tableRoot.innerHTML = '';
-//     const table = document.createElement('table');
-
-//     // create header
-//     const trHead = document.createElement('tr');
-//     for (let field of ['story_id', 'author', 'unix_time', 'score', 'title', '#comments', 'comments']) {
-//         const th = document.createElement('th')
-//         th.innerText = field;
-//         trHead.appendChild(th);
-//     }
-//     // add data
-//     table.appendChild(trHead);
-//     for (let storyId of Object.keys(data)) {
-//         const tr = document.createElement('tr');
-//         for (let field of ['story_id', 'author', 'unix_time', 'score', 'title', 'descendants']) {
-//             const td = document.createElement('td') 
-//             if (field === 'unix_time') {
-//                 const date = new Date(data[storyId][field] * 1000);
-//                 td.innerText = `${date.getFullYear()}/${date.getMonth()}/${date.getDate()}`;
-//             } else {
-//                 td.innerText = data[storyId][field];
-//             }            
-//             tr.appendChild(td);
-//         }
-//         // add comments as details
-//         const td = document.createElement('td');
-//         const details = getHTMLDetails('show', data[storyId]['children']);
-//         td.appendChild(details);
-//         tr.appendChild(td);
-        
-//         // add row to table
-//         table.appendChild(tr);
-//     }
-
-//     tableRoot.append(table);   
-// }
 
 // Event Listeners    
 for (let filterBy of ['date', 'id', 'comm', 'score']) {
@@ -266,7 +285,7 @@ for (let filterBy of ['date', 'id', 'comm', 'score']) {
 queryDbBtn.addEventListener('click', function (evt) {
     const data = {
         'show-id-begin-range': show.id.begin.range.value,
-        'show-id-end-range': show.id.end.range.value,
+        'show-id-end-range': Math.min(parseInt(show.id.begin.range.value) + idRange, parseInt(show.id.end.range.value)),
         'show-comm-begin-range': show.comm.begin.range.value,
         'show-comm-end-range': show.comm.end.range.value,
         'show-score-begin-range': show.score.begin.range.value,
@@ -277,9 +296,8 @@ queryDbBtn.addEventListener('click', function (evt) {
 
     postData('/db', data)
         .then(res => {
-            //console.log(res); // JSON data parsed by `data.json()` call
-            //addDataToTable(res);
             const table = getNewHNPostTable();
+            console.log(`Posting stuff from #${data['show-id-begin-range']} to #${data['show-id-end-range']}`);
             appendDataToHNPostTable(table, res);
         })
         .catch((err) => console.log(err));
