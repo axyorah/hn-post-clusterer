aefb465d1b81f0cee0caaef77ccf525e387dd451
axyorah <elvira.bozileva@gmail.com>
2021-09-28 21:43:09 +0200

    add: `BatchedPipeliner` now has `set_story_batches()` and `set_embedding_batches()` methods that don't take any inputs and don't return any outputs; instead they both internally set `self._stories` and `self._embeddings` vars to generators that are only used once to reduce the amount of times `copy_and_measure_generator()` needs to be called; copying/measuring generator appears to be slower than fetching data from db and generating embeddings on the fly...; `setialize_result()` currently doesn't work with setters!!!

--- flaskr/utils/generalutils.py
+++ flaskr/utils/generalutils.py
@@ -13,15 +13,11 @@ from flaskr.utils.formutils import (
 
 from flaskr.utils.semanticutils import (
     get_story_embeddings,
-    cluster_stories_with_faiss,
-    project_embeddings,
 
 )
 
 from flaskr.utils.dbutils import (
     get_stories_with_children_from_id_range,
-    get_id_list_from_sqlite_rows,
-    get_document_list_from_sqlite_rows,
     get_document_dict_from_sqlite_rows
 )
 
@@ -30,6 +26,7 @@ from flaskr.utils.clusterutils import (
 )
 
 # TODO: use `cycle` instead of `tee` for copying generators...
+# TODO: make 'measure' in 'copy_and_measure_generator' optional
 
 class BatchedPipeliner:
     def __init__(self, request):
@@ -39,9 +36,8 @@ class BatchedPipeliner:
 
         self._stories = None
         self._embeddings = None
+        self._labels = None
 
-        self.ids = []
-        self.labels = []
         self.kmeans = None
         self.centroids = None
         """
@@ -70,7 +66,7 @@ class BatchedPipeliner:
                 request_form['end_id'] = min(b_id + delta_id, end_id)
 
                 story_rows = get_stories_with_children_from_id_range(request_form) # list of sql rows
-                story_dicts = get_document_dict_from_sqlite_rows(story_rows) # dict of dicts
+                story_dicts = get_document_dict_from_sqlite_rows(story_rows, aslist=True) # list of dicts
 
                 # TODO: make it a list of dicts and adjust helpers in semanticutils.py
                 if len(story_dicts):
@@ -84,13 +80,38 @@ class BatchedPipeliner:
 
         return batches[1]
 
+    def set_story_batches(self, delta_id=10000):
+        """
+        doesn't return anything, but internally sets a generator, where each element 
+        is a list ("batch") of story dicts with the following fields:
+            'story_id', 'author', 'unix_time', 'score', 
+            'title', 'url', 'num_comments', 'children'
+        """
+        print('[INFO] fetching data from db...')
+        begin_id = int(self.request_form["begin_id"])
+        end_id = int(self.request_form["end_id"])
+
+        def batch_generator(begin_id, end_id, delta_id):
+            for b_id in range(begin_id, end_id, delta_id):
+                request_form = self.request_form
+                request_form['begin_id'] = b_id
+                request_form['end_id'] = min(b_id + delta_id, end_id)
+
+                story_rows = get_stories_with_children_from_id_range(request_form) # list of sql rows
+                story_dicts = get_document_dict_from_sqlite_rows(story_rows, aslist=True) # list of dicts
+
+                # TODO: make it a list of dicts and adjust helpers in semanticutils.py
+                if len(story_dicts):
+                    yield story_dicts
+        
+        # set genrator
+        self._stories = batch_generator(begin_id, end_id, delta_id)
+
     def get_embedding_batches(self, story_batches):
         print('[INFO] generating embeddings...')
         def batch_generator(story_batches):
             for batch in story_batches:
-                ids, embeds = get_story_embeddings(batch)
-                self.ids.append(ids)
-                yield embeds    
+                yield get_story_embeddings(batch)
         
         # copy genrator: save one copy, return another
         gen = batch_generator(story_batches)
@@ -100,24 +121,56 @@ class BatchedPipeliner:
 
         return batches[1]
 
-    def cluster_story_batches(self, embedding_batches, n_clusters=10):
-        print(f'[INFO] clustering stories to {n_clusters} clusters...')
-        # TODO: n_clusters should be accessed from form request
-        batches, (_,_) = copy_and_measure_generator(embedding_batches, num_copies=2)
+    def set_embedding_batches(self, story_batches=None):
+        """
+        doesn't return anything, but internally sets a generator,
+        where each element is a list of story embeddings
+        """
+        if story_batches is None and self._stories is None:
+            raise RuntimeError(
+                'There is nothing to embed yet! ' +\
+                'You must fetch stories from the database first! ' +\
+                'Consider running `get_story_batches()` or `set_story_batches()`'
+            )
+
+        print('[INFO] generating embeddings...')
+        def batch_generator(story_batches):
+            for batch in story_batches:
+                embeds = get_story_embeddings(batch)
+                yield embeds    
         
+        # set genrator
+        self._embeddings = batch_generator(story_batches or self._stories)
+
+    def cluster_story_batches(self, embedding_batches=None, n_clusters=10):
+        if embedding_batches is None and self._embeddings is None:
+            raise RuntimeError(
+                'There is nothing to cluster yet! ' +\
+                'You must obtain and story embeddings first! ' +\
+                'Consider running `get_embedding_batches()` or `set_embedding_batches()`'
+            )
+
+        # TODO: n_clusters should be accessed from form request
+        print('[INFO] copying embeddings')
+        batches, (_,_) = copy_and_measure_generator(
+            embedding_batches or self._embeddings, 
+            num_copies=2
+        )
+        
+        print(f'[INFO] clustering stories to {n_clusters} clusters...')
         self.kmeans = MiniBatchKMeans(n_clusters=n_clusters)        
         # train kmeans
         for batch in batches[0]:
             self.kmeans.partial_fit(batch)
 
-        # predict labels
-        for batch in batches[1]:
-            lbls = self.kmeans.predict(batch)
-            self.labels.append(lbls)
-
         self.centroids = self.kmeans.cluster_centers_
 
-        return self.labels
+        # predict labels
+        def cluster(batches):
+            for batch in batches:
+                yield self.kmeans.predict(batch)
+
+        self._labels = cluster(batches[1])
 
     def serialize_result(self, fname='./data/df.csv'):
         # check if all the necessary data is available
@@ -127,6 +180,7 @@ class BatchedPipeliner:
                 'You must obtain and cluster embeddings first!'
             )
 
+        print('[INFO] copying stories...')
         story_batches, (l,_) = copy_and_measure_generator(self._stories, num_copies=2)
         self._stories = story_batches[0]
         if not l:
@@ -134,7 +188,8 @@ class BatchedPipeliner:
                 'Story generator is empty! ' +\
                 'Consider rerunning .get_story_batches()'
             )
-
+        
+        print('[INFO] copying embeddings...')
         emb_batches, (l,_) = copy_and_measure_generator(self._embeddings, num_copies=3)
         self._embeddings = emb_batches[0]
         if not l:
@@ -143,7 +198,7 @@ class BatchedPipeliner:
                 'Consider rerunning .get_embedding_batches()'
             )
 
-        if not self.labels:
+        if not self._labels:
             raise RuntimeError(
                 'There are no labels to serialize! ' +\
                 'Consider running .cluster_story_batches()'
@@ -169,15 +224,15 @@ class BatchedPipeliner:
         with open(fname, 'w') as f:
             f.write('\t'.join(fields) + '\n')
 
-        for id_batch, st_batch, emb_batch, lbl_batch in zip(self.ids, story_batches[1], emb_batches[2], self.labels):
+        for st_batch, emb_batch, lbl_batch in zip(story_batches[1], emb_batches[2], self._labels):
             emb_proj = pca.transform(emb_batch)
-            for sid, emb, lbl in zip(id_batch, emb_proj, lbl_batch):
+            for st, emb, lbl in zip(st_batch, emb_proj, lbl_batch):
                 with open(fname, 'a') as f:
                     f.write(
                         '\t'.join([
-                            str(sid),
-                            st_batch[sid].get('title') or '',
-                            st_batch[sid].get('url') or '', # nullable
+                            str(st.get('story_id')),
+                            st.get('title') or '',
+                            st.get('url') or '', # nullable
                             str(lbl),
                             ','.join(str(val) for val in emb)
                         ]) + '\n'
