66fe51d0fe818fde3995286067a2bc5feb7b12f1
axyorah <elvira.bozileva@gmail.com>
2021-09-29 20:07:16 +0200

    add: added a tab for displaying posts from a specified cluster

--- flaskr/static/js/semantic_utils.js
+++ flaskr/static/js/semantic_utils.js
@@ -39,4 +39,41 @@ semanticClusterBtn.addEventListener('click', function (evt) {
         console.log(err);
         semanticClusterBtn.innerHTML = `Cluster Posts`;
     });
+});
+
+
+showSemanticClusterPostsBtn.addEventListener('click', function (evt) {
+    console.log('clicked show posts btn');
+    const targetLabel = showSemanticClusterPostsNum.value;
+
+    let filtered_ids;
+
+    // add in-progress animation
+    this.innerHTML = `${spinnerAmination} Querying DB...`;
+
+    // read all labels
+    postData('/file/readcsv', {
+        'sender': 'reader',
+        'fname': 'data/df.csv'    
+    }).then(res => {
+        // filter ids based on the target label and query db
+        filtered_ids = filterAbyBwhereBisC(
+            res.contents['id'], 
+            res.contents['label'], 
+            targetLabel
+        );
+        return postData('/db/get', {
+            'sender': 'kmeans-show',
+            'story_ids': filtered_ids,
+        })
+    }).then(res => {
+        // display filtered posts in a new table
+        console.log(res);
+        const table = getNewHNPostTable(); // no morePostsBtn!
+        appendDataToHNPostTable(table, res);
+        this.innerHTML = 'Show Posts';      
+    }).catch(err => {
+        console.log(err)
+        this.innerHTML = 'Show Posts';
+    })
 })
