d745e2f159bf2375726528d027fe9d93fb1e916d
axyorah <elvira.bozileva@gmail.com>
2021-10-06 16:36:10 +0200

    rm: removed `get_stories_from_db_and_serialize_ids_and_comments(.)` from `datautils.py` - no longer using it for anything...

--- flaskr/utils/datautils.py
+++ flaskr/utils/datautils.py
@@ -2,15 +2,7 @@ import os
 import json
 import numpy as np
 import bs4 as bs
-from flask import app
 from gensim.parsing.preprocessing import preprocess_documents
-
-from flaskr.utils.dbutils import (
-    get_stories_with_children_from_id_range,
-    get_id_list_from_sqlite_rows,
-    get_document_list_from_sqlite_rows,
-    get_document_dict_from_sqlite_rows
-)
 
 def create_file(fname):
     with open(fname, 'w') as f:
@@ -104,42 +96,3 @@ def serialize_dict_of_dicts(dct, fname='./data/dict.csv'):
             ) + '\n')
 
     return True
-
-
-def get_stories_from_db_and_serialize_ids_and_comments(corpus_dir, form_request, delta_id=10000):
-    """
-    query db in portions(!) to get stories with all child comments,
-    tokenize comments and serialize them on disk:
-    all child comments corresponding to the same story are serialized 
-    as a single line of ' '-separated tokens in specified file;
-    
-    corpus_dir: string: name of a directory to store comment and id files
-    form_request: dict[]: parsed form request
-    delta_id: int: id range processed in a single db query
-    """
-    # create new files for storing story ids and comments
-    fname_ids = os.path.join(corpus_dir, 'ids.txt')
-    fname_comments = os.path.join(corpus_dir, 'corpus.txt')
-
-    os.makedirs(corpus_dir, exist_ok=True) 
-    create_file(fname_ids)
-    create_file(fname_comments)
-
-    # query db in portions - `delta_id` entries at a time
-    begin_id = int(form_request["begin_id"])
-    end_id = int(form_request["end_id"])
-    for b_id in range(begin_id, end_id, delta_id):
-        # get current(!) begin_id and end_id range (b_id and e_id)
-        e_id = min(b_id + delta_id - 1, end_id)
-        form_request["begin_id"] = b_id
-        form_request["end_id"] = e_id
-        print(f"serializing from {b_id} to {e_id}")
-
-        # query db for a portion of data and... 
-        story_rows = get_stories_with_children_from_id_range(form_request)        
-        ids = get_id_list_from_sqlite_rows(story_rows)
-        comments = get_document_list_from_sqlite_rows(story_rows)
-        
-        # ... store all ids and comments in specified files
-        serialize_raw_documents_to_disc(fname_ids, ids)
-        serialize_tokenized_documents_to_disc(fname_comments, comments)
