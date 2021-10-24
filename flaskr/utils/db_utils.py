653daa9264f29011d55462d857955f0427eff299
axyorah <elvira.bozileva@gmail.com>
2021-10-24 23:41:34 +0200

    add: added `Story` and `Comment` classes that share the functionality of `DBHelper` but have somewhat cleaner/more intuitive api

--- flaskr/utils/db_utils.py
+++ flaskr/utils/db_utils.py
@@ -55,15 +55,22 @@ def fetch_and_add_item_by_id(item_id: Union[int, str], commit: str = True) -> Op
 
     # skip if comment and already in db
     # keep is story and already in db -> update it later
+    # story_needs_update = False
+    # if DBHelper.find_comment_by_id(item_id):
+    #     print('')
+    #     return
+    # if DBHelper.find_story_by_id(item_id):
+    #     story_needs_update = True
+
     story_needs_update = False
-    if DBHelper.find_comment_by_id(item_id):
-        print('')
-        return
-    if DBHelper.find_story_by_id(item_id):
+    if Comment.find_by_id(item_id):
+        print('already in db, skipping...')
+        return 
+    if Story.find_by_id(item_id):
         story_needs_update = True
 
     # get item
-    item = query_api(item_id) # raw response
+    item = query_api(item_id) # raw re  sponse
     item = translate_response_api2schema(item) # fields are now the same as in schema (+)
     
     # skip if empty/deleted/dead
@@ -79,11 +86,17 @@ def fetch_and_add_item_by_id(item_id: Union[int, str], commit: str = True) -> Op
     )
 
     if story_needs_update:
-        DBHelper.update_story_in_db(item, commit=commit)
+        #DBHelper.update_story_in_db(item, commit=commit)
+        story = Story(**item)
+        story.update()
     elif item.get('type') == 'story':
-        DBHelper.add_story_to_db(item, commit=commit)
+        #DBHelper.add_story_to_db(item, commit=commit)
+        story = Story(**item)
+        story.add()
     elif item.get('type') == 'comment':
-        DBHelper.add_comment_to_db(item, commit=commit)
+        #DBHelper.add_comment_to_db(item, commit=commit)
+        comment = Comment(**item)
+        comment.add()
 
     return item
 
@@ -115,23 +128,23 @@ def query_hn_and_add_result_to_db(form_request: Dict) -> None:
 
 class DBHelper:
     STORY_FIELDS = [
-        'story_id', 
-        'author', 
-        'unix_time', 
-        'body', 
-        'url', 
-        'score', 
-        'title', 
-        'num_comments', 
-        'comment_embedding'
+        "story_id", 
+        "author", 
+        "unix_time", 
+        "body", 
+        "url", 
+        "score", 
+        "title", 
+        "num_comments", 
+        "comment_embedding"
     ]
 
     COMMENT_FIELDS = [
-        'comment_id',
-        'author',
-        'unix_time',
-        'body',
-        'parent_id'
+        "comment_id",
+        "author",
+        "unix_time",
+        "body",
+        "parent_id"
     ]
 
     STORY_PATTERN_WITHOUT_WHERE = """
@@ -324,3 +337,261 @@ class DBHelper:
             db.commit()
         cls.close_connection()
         return True
+
+class Story:
+    SCHEMA = [
+        "story_id", 
+        "author", 
+        "unix_time", 
+        "body", 
+        "url", 
+        "score", 
+        "title", 
+        "num_comments", 
+        "comment_embedding"
+    ]
+
+    RECURSIVE_CTE_WITHOUT_WHERE = """
+        WITH RECURSIVE tab(id, parent_id, root_id, level, title, body) AS (
+            SELECT 
+                s.story_id, 
+                s.story_id,
+                s.story_id,
+                1,
+                s.title,
+                s.title
+            FROM story AS s
+
+            UNION
+
+            SELECT
+                c.comment_id, 
+                c.parent_id,
+                tab.root_id,
+                tab.level + 1,
+                c.author,
+                c.body
+            FROM tab, comment as c WHERE c.parent_id = tab.id
+        ) 
+
+        SELECT 
+            s.story_id,
+            s.author, 
+            s.unix_time,
+            s.score,
+            s.title, 
+            s.url,
+            s.num_comments,
+            s.comment_embedding,
+            (
+                SELECT COALESCE(GROUP_CONCAT(body, "<br><br>"), " ")
+                FROM tab
+                WHERE root_id = s.story_id
+                GROUP BY root_id
+            ) AS children
+        FROM story AS s
+        """
+
+    def __init__(self, 
+        story_id=None, author=None, unix_time=None, 
+        title=None, score=None, num_comments=None,
+        body=None, url=None, comment_embedding=None, children=None,
+        **kwargs # non-schema api kwargs, e.g., `deleled`, `type`, ...
+    ):
+        self.story_id = story_id
+        self.author = author
+        self.unix_time = unix_time
+        self.title = title
+        self.score = score
+        self.num_comments = num_comments
+        self.body = body
+        self.url = url
+        self.comment_embedding = comment_embedding
+        self.children = children
+
+    def json(self) -> Dict:
+        return {
+            "story_id": self.story_id,
+            "author": self.author,
+            "unix_time": self.unix_time,
+            "title": self.title,
+            "score": self.score,
+            "num_comments": self.num_comments,
+            "body": self.body,
+            "url": self.url,
+            "comment_embedding": self.comment_embedding,
+            "children": self.children
+        }
+
+    @classmethod
+    def find_by_id(cls, story_id: int) -> Optional['Story']:
+        get_query = """
+            SELECT * FROM story WHERE story_id = ?
+        """
+        rows = DBHelper.get_query(get_query, [story_id])
+        if rows:
+            return  cls(**DBHelper.rows2dicts(rows)[0])
+
+    @classmethod
+    def find_by_id_with_children(cls, story_id: int) -> Optional['Story']:
+        get_query = f"""
+            {cls.RECURSIVE_CTE_WITHOUT_WHERE}
+            WHERE s.story_id = ?
+        """
+        rows = DBHelper.get_query(get_query, [story_id])
+        if rows:
+            return cls(**DBHelper.rows2dicts(rows)[0])
+
+    def add(self) -> None:
+        # add to story table
+        add_story_query = f"""
+            INSERT INTO story
+            ({', '.join(self.SCHEMA)})
+            VALUES ({', '.join(['?' for _ in self.SCHEMA])});
+        """
+        story_params = [getattr(self, field) for field in self.SCHEMA]
+        DBHelper.mod_query(add_story_query, story_params)
+
+        # add to parent table
+        add_parent_query = """
+            INSERT INTO parent
+            (parent_id, parent_type)
+            VALUES (?, ?)
+        """
+        parent_params = (self.story_id, 'story')
+        DBHelper.mod_query(add_parent_query, parent_params)
+
+    def update(self) -> None:
+        update_query = f"""
+            UPDATE story
+            SET 
+                author = ?, 
+                unix_time = ?, 
+                body = ?, 
+                url = ?, 
+                score = ?, 
+                title = ?, 
+                num_comments = ?,
+                comment_embedding = ?
+            WHERE story_id = ?;
+        """
+        params = [
+            self.author,
+            self.unix_time,
+            self.body,
+            self.url,
+            self.score,
+            self.title,
+            self.num_comments,
+            self.comment_embedding,
+            self.story_id
+        ]
+        DBHelper.mod_query(update_query, params)
+
+    def delete(self) -> None:
+        delete_query = """
+            DELETE FROM story WHERE story_id = ?
+        """
+        DBHelper.mod_query(delete_query, [self.story_id])
+
+class Comment:
+    SCHEMA = [
+        "comment_id",
+        "author",
+        "unix_time",
+        "body",
+        "parent_id"
+    ]
+    
+    def __init__(self, 
+        comment_id=None, author=None, unix_time=None, body=None, parent_id=None, 
+        **kwargs # non-schema api kwargs, e.g., `deleled`, `type`, ...
+    ):
+        self.comment_id = comment_id
+        self.author = author
+        self.unix_time = unix_time
+        self.body = body
+        self.parent_id = parent_id
+
+    def json(self) -> Dict:
+        return {
+            "comment_id": self.comment_id,
+            "author": self.author,
+            "unix_time": self.unix_time,
+            "body": self.body,
+            "parent_id": self.parent_id
+        }
+
+    @classmethod
+    def find_by_id(cls, comment_id: int) -> Optional['Comment']:
+        get_query = """
+            SELECT * FROM comment WHERE comment_id = ?
+        """
+        rows = DBHelper.get_query(get_query, [comment_id])
+        if rows:
+            return cls(**DBHelper.rows2dicts(rows)[0])
+
+    def add(self) -> None:
+        # add to comment table
+        add_query = f"""
+            INSERT INTO comment
+            ({', '.join(self.SCHEMA)})
+            VALUES ({', '.join(['?' for _ in self.SCHEMA])});
+        """
+        params = [getattr(self, field) for field in self.SCHEMA]
+        DBHelper.mod_query(add_query, params)
+
+        # add to parent table
+        add_parent_query = """
+            INSERT INTO parent
+            (parent_id, parent_type)
+            VALUES (?, ?)
+        """
+        parent_params = (self.comment_id, 'comment')
+        DBHelper.mod_query(add_parent_query, parent_params)
+
+    def update(self) -> None:
+        update_query = f"""
+            UPDATE comment
+            SET 
+                author = ?, 
+                unix_time = ?, 
+                body = ?, 
+                parent_id = ?
+            WHERE comment_id = ?;
+        """
+        params = [
+            self.author,
+            self.unix_time,
+            self.body,
+            self.comment_id
+        ]
+        # TODO: update parent table???
+        DBHelper.mod_query(update_query, params)
+
+    def delete(self) -> None:
+        delete_query = """
+            DELETE FROM comment WHERE comment_id = ?
+        """
+        DBHelper.mod_query(delete_query, [self.comment_id])
+
+class StoryList:
+    @classmethod
+    def find_by_ids(cls, id_list: List[int]) -> List[Story]:
+        get_query = f"""
+            SELECT * FROM story 
+            WHERE story_in IN ({', '.join('?' for _ in id_list)})
+        """
+        rows = DBHelper.get_query(get_query, id_list)
+        stories = DBHelper.rows2dicts(rows)
+        return [Story(**story) for story in stories] # or leave dict
+
+    @classmethod
+    def find_by_ids_with_children(cls, id_list: List[int]) -> List[Story]:
+        get_query = f"""
+            {Story.RECURSIVE_CTE_WITHOUT_WHERE} 
+            WHERE story_in IN ({', '.join('?' for _ in id_list)})
+        """
+        rows = DBHelper.get_query(get_query, id_list)
+        stories = DBHelper.rows2dicts(rows)
+        return [Story(**story) for story in stories]
