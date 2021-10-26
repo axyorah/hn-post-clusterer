73015930d56b2367ebc1eeb06840a43065e7405b
axyorah <elvira.bozileva@gmail.com>
2021-10-26 20:47:50 +0200

    add: added a small debug helper

--- flaskr/utils/date_utils.py
+++ flaskr/utils/date_utils.py
@@ -5,6 +5,20 @@ import time
 URL_ID = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'
 URL_MAXID = 'https://hacker-news.firebaseio.com/v0/maxitem.json?print=pretty'
 
+def check(year, month, day, item_id):
+    def helper(item_id):
+        res = rq.get(URL_ID.format(item_id))
+        while not res.ok or not res.json().get('time'):
+            res = rq.get(URL_ID.format(item_id + 1))
+        return res.json()['time']
+
+    ts = helper(item_id)
+    d = datetime.datetime.fromtimestamp(ts)    
+    print(f'first id on {year}/{month}/{day}: {item_id} ({d.year}/{d.month}/{d.day})')            
+
+    return year == d.year and month == d.month and day == d.day
+      
+
 def date2ts(year, month, day):
     date = datetime.date(year=year, month=month, day=day)
     timetpl = date.timetuple()
@@ -24,9 +38,10 @@ def get_first_id_on_day(year, month, day):
     while lo <= hi:
         mi = (lo + hi) // 2        
         ts = helper(mi)
-        if ts > target_ts:
-            hi = mi - 1
-        else:
+        if target_ts > ts:
             lo = mi + 1
-            
+        else:
+            hi = mi - 1
+
+    #print(check(year, month, day, hi+1))          
     return hi + 1
