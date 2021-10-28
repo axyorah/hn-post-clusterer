b55648e113be179099df78dac16634cd168fece3
axyorah <elvira.bozileva@gmail.com>
2021-10-28 18:35:28 +0200

    mod: running `bash run.sh` now also activates python venv if `venv` dir is present

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
+# set env vars
 export FLASK_APP=flaskr
 export FLASK_ENV=development
+
+# activate python venv if venv directory exists
+if [[ -d venv ]]
+    then source venv/bin/activate
+fi
+
+# run app
 flask init-db
 flask run
