89bac2f99df7c9d7c3779b08b2e5c65e68c7747f
axyorah <elvira.bozileva@gmail.com>
2021-12-13 01:41:25 +0100

    mod: first sketch of prettified cluster pipe

--- flaskr/utils/general_utils1.py
+++ flaskr/utils/general_utils1.py
@@ -1,3 +1,4 @@
+from types import FunctionType, prepare_class
 from typing import Any, Dict, List, Optional, Generator
 
 import os
@@ -24,19 +25,145 @@ from flaskr.utils.cluster_utils import (
 from flaskr.utils.db_utils import DBHelper as dbh
 from flaskr.models.story import Story
 
-def rebatch_generator(batches: Generator, min_batch_size: int):
-    """
-    resamples generator of batches so that 
-    new batches have atleast `min_batch_size` elements
-    """
-    prev, curr = [], []
-    for batch in batches:
-        curr += batch
-        if len(curr) >= min_batch_size:
-            if prev:
-                yield prev
-            prev = curr.copy()
-            curr = []
+class Pipeliner:
+    def __init__(self):
+        self.pipe = []
 
-    # last batch
-    yield prev + curr
+    def add(self, fun: FunctionType, params: Optional[Dict]=None):
+        self.pipe.append((fun, params or {}))
+
+    def run(self, inpt):
+        x = inpt
+        for fun, params in self.pipe:
+            x = fun(x, **params)
+        return x
+
+
+class Batcher:
+    @classmethod
+    def batch_generator(cls, query, params):
+        pass
+
+    @classmethod
+    def rebatch_generator(cls, batches: Generator, min_batch_size: int):
+        """
+        resamples generator of batches so that 
+        new batches have atleast `min_batch_size` elements
+        """
+        prev, curr = [], []
+        for batch in batches:
+            curr += batch
+            if len(curr) >= min_batch_size:
+                if prev:
+                    yield prev
+                prev = curr.copy()
+                curr = []
+
+        # last batch
+        yield prev + curr
+
+
+class Serializer:
+    # observer
+    pass
+
+
+class ClustererBuilder:
+    def __init__(self, clusterer):
+        self.clusterer = clusterer
+
+    def n_clusters(self, val):
+        self.clusterer._n_clusters = val
+        return self
+
+    def n_pca_dims(self, val):
+        self.clusterer._n_pca_dims = val
+        return self
+
+    def min_batch_size(self, val):
+        self.clusterer._min_batch_size = val
+        return self
+
+    def begin_timestep(self, val):
+        self.clusterer._begin_ts = val
+        return self
+
+    def end_timestep(self, val):
+        self.clusterer._end_ts = val
+        return self
+
+    def begin_comments(self, val):
+        self.clusterer._begin_comm = val
+        return self
+
+    def end_comments(self, val):
+        self.clusterer._end_comm = val
+        return self
+
+    def begin_score(self, val):
+        self.clusterer._begin_score = val
+        return self
+
+    def end_score(self, val):
+        self.clusterer._end_score = val
+        return self
+
+    def build(self):
+        return self.clusterer
+
+
+class Clusterer:
+    def __init__(self):
+        self._n_clusters = 10
+        self._model_name = 'sentence-transformers/all-distilroberta-v1'
+
+        self._n_pca_dims = 100
+        self._min_batch_size = 100
+
+        self._num_batches = 0
+        self._num_stories = 0
+
+        self._stories = None
+        self._embeddings = None
+        self._labels = None
+
+        self.pipeliner = Pipeliner()
+        self.embedder = StoryEmbedder(model_name=self._model_name)
+        self.scaler = BatchedGeneratorStandardizer()
+        self.kmeans = MiniBatchKMeans(n_clusters=self._n_clusters)
+        self.pca = None
+        self.centroids = None
+
+    @property
+    def set(self):
+        return ClustererBuilder(self)
+
+    def _get_story_batches(self):
+        pass
+
+    def _get_or_generate_story_embeddings(self):
+        # single batch
+        pass
+
+    def _get_embedding_batches(self):
+        pass
+
+    def _standardize_embedding_batches(self):
+        pass
+
+    def _reduce_embedding_diimensionality(self):
+        pass
+
+    def _cluster_batches(self):
+        pass
+
+    def run(self):
+        self.pipeliner.add(self._get_story_batches)
+        self.pipeliner.add(self._get_embedding_batches)
+        self.pipeliner.add(self._standardize_embedding_batches)
+        self.pipeliner.add(self._reduce_embedding_diimensionality)
+        self.pipeliner.add(self._cluster_batches)
+
+        output = self.pipeliner.run()
+
+    
